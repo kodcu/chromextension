6e89dd741fc5f74699dd529dafbcf77480616d73
ozler.hakan <ozler.hakan@gmail.com>
2014-07-16 17:48:58 +0300

    new properties come with 2.1.0

--- js/background.js
+++ js/background.js
@@ -1,59 +1,61 @@
 ﻿(function ($) {
     $(document).ready(function () {
         try {
-            var firstItemNames, secondItemNames;
+            var firstItemNames, secondItemNames;            
             //pick a random number as a timer   
             var random = Math.floor((Math.random() * 2600000) + 1000000);
-           
+
             if (!localStorage.isInitialized) {
                 localStorage.isActivated = true;
                 localStorage.isCountChange = false;
             }
 
             function init() {
-                if (JSON.parse(localStorage.isActivated)) { getFirst(); }
-                
+                if (JSON.parse(localStorage.isActivated)) { getFirstAttempt(); }
+
                 setInterval(function () {
-                    if (JSON.parse(localStorage.isActivated)) {                        
+                    if (JSON.parse(localStorage.isActivated)) {
                         if (JSON.parse(localStorage.isCountChange)) {
-                            getFirst();
+                            getFirstAttempt();
                             localStorage.isCountChange = false;
                         }
-                        getSecond();                       
+                        getSecondAttempt();
                     }
                 }, random);
             }
             init();
 
-            function getFirst() {
+            function getFirstAttempt() {
                 firstItemNames = [];
-                var req = new XMLHttpRequest();
-                req.open("GET", "http://www.kodcu.com/feed/", true);
-                req.onreadystatechange = listof;
-                req.send();
+
+                var req = HttpRequest();
+                req.onload = listof;
+                req.send(null);
+
                 function listof() {
-                    if (req.readyState == 4 && req.status == 200) {                     
+                    if (req.readyState == 4 && req.status == 200) {
                         var rss = $.parseXML(req.responseText);
                         $(rss).find("item").each(function (index, item) {
                             firstItemNames[index] = $(item).find("title").text();
-                        });                        
+                        });
                     }
                 }
             }
 
-            function getSecond() {
+            function getSecondAttempt() {
                 secondItemNames = [];
-                var req = new XMLHttpRequest();
-                req.open("GET", "http://www.kodcu.com/feed/", true);
-                req.onreadystatechange = listof;
-                req.send();
+
+                var req = HttpRequest();
+                req.onload = listof;
+                req.send(null);
+
                 function listof() {
                     if (req.readyState == 4 && req.status == 200) {
-                            var rss = $.parseXML(req.responseText);
-                            $(rss).find("item").each(function (index, item) {
-                                secondItemNames[index] = $(item).find("title").text();
-                            });
-                            controlItems();                        
+                        var rss = $.parseXML(req.responseText);
+                        $(rss).find("item").each(function (index, item) {
+                            secondItemNames[index] = $(item).find("title").text();
+                        });
+                        controlItems();
                     }
                 }
             }
@@ -62,7 +64,7 @@
                 var count = 0;
                 if (JSON.parse(localStorage.isCountChange) == false &&
                 firstItemNames.length == secondItemNames.length) {
-                    
+
                     for (var i = 0; i < secondItemNames.length; i++) {
                         for (var y = 0; y < firstItemNames.length; y++) {
                             if (secondItemNames[i] == firstItemNames[y]) {
@@ -72,14 +74,22 @@
                         }
                     }
 
+                    //inform users for the new post(s)
                     if (count != secondItemNames.length) {
                         var result = secondItemNames.length - count;
                         chrome.browserAction.setBadgeText({
                             text: result.toString()
                         });
                     }
-
                 }
+            }
+
+            function HttpRequest() {
+                var req = new XMLHttpRequest();
+                req.open("GET", "http://www.kodcu.com/feed/", true);
+                req.setRequestHeader("Pragma", "no-cache");
+                req.setRequestHeader("Cache-Control", "no-cache");
+                return req;
             }
 
         } catch (err) {
--- js/main.js
+++ js/main.js
@@ -1,22 +1,69 @@
-(function ($) {
+﻿(function ($) {
     $(document).ready(function () {
         try {
-                        
-            function ClickListen() {
-                $('a.more-link,a.catehref,h1 a,ul li a').live('click', function () {
-                    chrome.tabs.create({
-                        'windowId': chrome.windows.WINDOW_ID_CURRENT,
-                        'url': $(this).attr('href'),
-                        'selected': true
-                    });
+            var storedBooks = true;
+
+            $('a.more-link,a.catehref,h1 a,ul li a').live('click', function () {
+                chrome.tabs.create({
+                    'windowId': chrome.windows.WINDOW_ID_CURRENT,
+                    'url': $(this).attr('href'),
+                    'selected': true
                 });
+            });
+
+            $('.starBookmark').live('click', function () {
+                var source = $(this).attr('source');
+                var access = $(this).attr('access');
+                //if the requested bookmark has not been already marked!
+                if (access == "false") {
+                    chrome.bookmarks.create({
+                        'title': $(this).attr('booktitle'),
+                        'url': source
+                    },
+                    function (newFolder) {
+                        console.log("New bookmark:" + newFolder.title);
+                    });
+                                       
+                    $(this).attr("access", "true");
+                    $(this).attr("title", "Bu haberi yıldıza tekrar tıklayarak yer iminizden çıkartabilirsiniz");
+                    $(this).css({ 'background': 'url(/images/star.png) 0 50% no-repeat' });
+                }
+                else {
+                    //delete the intended bookmark from (others bookmarks) section
+                    var bookmarkTreeNodes = chrome.bookmarks.getTree(
+                    function (bookmarkTreeNodes) {
+                        walkTreeNodes(bookmarkTreeNodes, source);
+                    });
+
+                    $(this).attr("access", "false");
+                    $(this).attr("title", "Bu haberi yer iminize ekleyin");
+                    $(this).css({ 'background': 'url(/images/unstar.gif) 0 50% no-repeat' });
+                }
+            });
+
+            function walkTreeNodes(bookmarkNodes, source) {
+                var i;
+                for (i = 0; i < bookmarkNodes.length; i++) {
+                    deleteNode(bookmarkNodes[i], source);
+                }
             }
-            ClickListen();
+
+            function deleteNode(bookmarkNode, source) {
+                if (bookmarkNode.title) {
+                    //delete the bookmark
+                    if (String(bookmarkNode.url) == source) {
+                        chrome.bookmarks.remove(String(bookmarkNode.id));
+                        console.log("The bookmark has been deleted!");
+                    }
+                }
+                if (bookmarkNode.children && bookmarkNode.children.length > 0) {
+                    walkTreeNodes(bookmarkNode.children, source);
+                }
+            }
 
             function updateIcon() {
                 if (localStorage.isCountChange != undefined &&
                 JSON.parse(localStorage.isCountChange) == false) {
-
                     chrome.browserAction.setBadgeText({ text: "" });
                     localStorage.isCountChange = true;
                 }
@@ -25,37 +72,49 @@
             updateIcon();
 
             var GetArticles = function () {
+
                 var req = new XMLHttpRequest();
                 req.open("GET", "http://www.kodcu.com/feed/", true);
+                req.setRequestHeader("Pragma", "no-cache");
+                req.setRequestHeader("Cache-Control", "no-cache");
                 req.onload = listof;
-                req.send(null);
+                req.send();
 
                 function listof() {
                     $listAll = "";
                     if (req.readyState == 4 && req.status == 200) {
-                
+
                         var response = req.responseText;
                         var rss = $.parseXML(response);
                         $(rss).find("item").each(function (index, item) {
                             var $listcate = "";
                             var count = 0;
                             var $desc = $(item).find("description").text();
+                            var href = $(item).find("link").text();
+
+                            var date = $.trim($(item).find("pubDate").text().substr(4, 12));
+                            var listDate = date.split(" ");
+                            var month = getMonthAbbreviation(listDate[1]);
                             var img = $(item).find("description").text().substr(0, $desc.indexOf('>') + 1);
+
                             $(item).find("category").each(function (indexs, items) {
                                 $listcate += '<a class="catehref" style="margin-right:1px;margin-left:1px;" href=http://www.kodcu.com/icerik/' + $(items).text() + ' title="' + $(items).text() + ' kategorisindeki t&#252;m yaz&#305;lar&#305; g&#246;ster" rel="category tag">' + $(items).text() + '</a> // ';
                                 if (++count == 5)
                                     return false;
-                            });
+                            });                            
+
                             $listAll += '<div class="postBoxMid">' +
                             '<div class="postBoxMidInner first clearfix">' +
-                            '<div class="date"><span>' + $(item).find("pubDate").text().substr(4, 12) + '</span></div>' +
-                            '<div class="category"> <div style="margin-left: 15px;width:288px">' + $listcate.substr(0, $listcate.length - 3) + '</div></div>' +
-                            '<h1> <a href="' + $(item).find("link").text() + '">' + $(item).find("title").text() + '</a></h1>' +
+                            '<div class="date"><span>' + listDate[0] + " " + month + " " + listDate[2] + '</span></div>' +
+                            '<div class="category"> ' +
+                            '<div class="starBookmark" source="' + href + '" booktitle="' + $(item).find("title").text() + '" access="" title="" index="' + index + '"></div>' +
+                            '<div style="margin-left: 15px;width:288px">' + $listcate.substr(0, $listcate.length - 3) + '</div></div>' +
+                            '<h1> <a href="' + href + '">' + $(item).find("title").text() + '</a></h1>' +
                             '<div class="postThumb"><img height="100" style="max-width:200px;" src="' + $(img).attr('src') + '"></div>' +
                             '<div class="textPreview"><p>' + $desc.substr($desc.indexOf('>') + 1) + '</p>' +
                             '</div>' +
                             '<div class="postMeta">' +
-                    '<a style="float: left; margin-top: 5px;margin-right: 6px;margin-top: 2px;" href="' + $(item).find("link").text() + '" class="more-link">Devam&#305;n&#305; Oku &#187;</a>' +
+                            '<a style="float: left; margin-top: 5px;margin-right: 6px;margin-top: 2px;" href="' + $(item).find("link").text() + '" class="more-link">Devam&#305;n&#305; Oku &#187;</a>' +
                                 '<div class="metaRight">' +
                                     '<img style="float: left; position: relative;;margin-top: -8px;" src="http://www.kodcu.com/wp/wp-content/themes/alltuts/images/ico_author.png" alt="Yazar">' +
                                     '<div style="margin-top:2px; margin-right: 0px;">' +
@@ -64,19 +123,89 @@
                             '</div>' +
                             '</div>' +
                             '</div>';
+
+                            setBookmarks(href,index);
                         });
                         $('.headItem').css({ 'display': 'block' });
                         $('.articles').html($listAll);
                         document.body.style.height = "100%";
                         console.log("Kodcu.com");
                     }
-                    
+                    else {
+                        console.log(req.status);
+                    }
                 }
-                
             }
             var articles = GetArticles();
-            
-           
+
+            function setBookmarks(href,index) {
+                chrome.bookmarks.search(href, function (bookmarkTreeNodes) {
+                    var book = {};
+                    if (bookmarkTreeNodes.length > 0) {
+                        book = {
+                            title: "Bu haberi yıldıza tekrar tıklayarak yer iminizden çıkartabilirsiniz",
+                            access: "true",
+                            cssStyle: "background: url(/images/star.png) 0px 50% no-repeat;",
+                            indexElem: index
+                        };
+                    }
+                    else {
+                        book = {
+                            title: "Bu haberi yer iminize ekleyin",
+                            access: "false",
+                            cssStyle: "background: url(/images/unstar.gif) 0px 50% no-repeat;",
+                            indexElem: index
+                        };
+                    }
+
+                    $(".starBookmark[index='" + book.indexElem + "']").attr("access", book.access);
+                    $(".starBookmark[index='" + book.indexElem + "']").attr("title", book.title);
+                    $(".starBookmark[index='" + book.indexElem + "']").attr("style", book.cssStyle);
+
+                });
+            }
+            function getMonthAbbreviation(month) {
+                switch (month) {
+                    case "Jan":
+                        return "Oca";
+                        break;
+                    case "Feb":
+                        return "&#350;ub";
+                        break;
+                    case "Mar":
+                        return "Mart";
+                        break;
+                    case "Apr":
+                        return "Nis";
+                        break;
+                    case "May":
+                        return "May";
+                        break;
+                    case "Jun":
+                        return "Haz";
+                        break;
+                    case "Jul":
+                        return "Tem";
+                        break;
+                    case "Aug":
+                        return "A&#287;u";
+                        break;
+                    case "Sep":
+                    case "Sept":
+                        return "Eyl";
+                        break;
+                    case "Oct":
+                        return "Eki";
+                        break;
+                    case "Nov":
+                        return "Kas";
+                        break;
+                    case "Dec":
+                        return "Ara";
+                        break;
+                }
+            }
+
         } catch (err) {
             console.log(err);
         }
