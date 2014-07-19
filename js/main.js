c49c42812f54a8aacc5de641678cf2a4ce83e58a
ozler.hakan <ozler.hakan@gmail.com>
2014-07-19 15:09:55 +0300

    version 2.1.1 (social websites are available)

--- js/main.js
+++ js/main.js
@@ -22,7 +22,7 @@
                     function (newFolder) {
                         console.log("New bookmark:" + newFolder.title);
                     });
-                                       
+
                     $(this).attr("access", "true");
                     $(this).attr("title", "Bu haberi yıldıza tekrar tıklayarak yer iminizden çıkartabilirsiniz");
                     $(this).css({ 'background': 'url(/images/star.png) 0 50% no-repeat' });
@@ -97,6 +97,17 @@
                             '<div class="category"> ' +
                             '<div class="starBookmark" source="' + post.href + '" booktitle="' + post.title + '" access="" title="" index="' + index + '"></div>' +
                             '<div style="margin-left: 15px;width:288px">' + post.getCategories() + '</div></div>' +
+                            '<div style="margin-top: 5px;margin-bottom: 5px;">' +
+                            //facebook
+                            '<iframe src="' + post.getFaceURL + '" ' +
+                            'scrolling="no" frameborder="0" style="border:none; overflow:hidden; height:21px; border: none; ' +
+                            'overflow: hidden; height: 21px;  margin-top: 8px; width: 89px; margin-left:0px;" allowTransparency="true"></iframe>' +
+                            //twitter
+                            '<iframe allowtransparency="true" frameborder="0" scrolling="no" src="' + post.getTwitURL + '" style="border:0px;width:95px; height:21px;"></iframe>' +
+                            //google+
+                            '<iframe src="' + post.getGoogleURL + '" allowtransparency="true" frameborder="0" scrolling="no" style="width: 95px;height: 21px; border:0;"'+
+                            'marginheight="0" marginwidth="0" frameborder="0" scrolling="no"  title="+1"></iframe>' +
+                            '</div>' +
                             '<h1> <a href="' + post.href + '">' + post.title + '</a></h1>' +
                             '<div class="postThumb"><img height="100" style="max-width:200px;" src="' + $(post.image).attr('src') + '"></div>' +
                             '<div class="textPreview"><p>' + post.getDescription() + '</p>' +
@@ -109,7 +120,7 @@
                                         '<a style="text-decoration:underline;">' + post.creator + '</a> taraf&#305;ndan yaz&#305;ld&#305;.</div>' +
                                 '</div>' +
                             '</div>' +
-                            '</div>' +                            
+                            '</div>' +
                             '</div>';
 
                             setBookmarks(post.href, index);
@@ -117,6 +128,8 @@
                         $('.headItem').css({ 'display': 'block' });
                         $('.articles').html(postHTML);
                         document.body.style.height = "100%";
+                        var html = document.getElementsByTagName('html');
+                        $(html).css('background-image', 'none');
                         console.log("Kodcu.com");
                     }
                     else {
@@ -127,13 +140,13 @@
             var articles = GetArticles();
 
             // an article object to specify each post that returns from the rss.
-            var article = function(post,hashtable) {
+            var article = function (post, hashtable) {
 
                 this.creator = post.find("creator").text();
                 this.title = post.find("title").text();
                 this.href = post.find("link").text();
                 var description = post.find("description").text();
-                this.image = description.substr(0, description.indexOf('>') + 1);                
+                this.image = description.substr(0, description.indexOf('>') + 1);
 
                 this.getDescription = function () {
                     return description.substr(description.indexOf('>') + 1);
@@ -164,6 +177,18 @@
                     });
                     return listCategories.substr(0, listCategories.length - 3);
                 }
+
+                this.getEncodedURL = function () {
+                    //replace ':' and '/'                    
+                    return this.href.replace(/:/g, "%3A").replace(/\//g, "%2F");
+                }
+
+                this.getFaceURL = "http://www.facebook.com/plugins/like.php?href=XXXXXXX&amp;width=21&amp;layout=button_count&amp;action=like&amp;show_faces=false&amp;share=false&amp;height=21&amp;appId=229499750441210".replace("XXXXXXX", this.getEncodedURL());
+
+                this.getTwitURL = "https://platform.twitter.com/widgets/tweet_button.html?text=YYYYY&url=XXXXXXX".replace("XXXXXXX", this.getEncodedURL()).replace("YYYYY",this.title);
+
+                this.getGoogleURL = "https://plusone.google.com/_/+1/fastbutton?usegapi=1&size=medium&hl=en-US&url=XXXXXXX&gsrc=3p&ic=1&jsh=m%3B%2F_%2Fscs%2Fapps-static%2F_%2Fjs%2Fk%3Doz.gapi.en.ujUWkKAfC4A.O%2Fm%3D__features__%2Fam%3DAQ%2Frt%3Dj%2Fd%3D1%2Ft%3Dzcms%2Frs%3DAItRSTNvVCaXXg92G3Y4v6FEA18mJwCnsA#_methods=onPlusOne%2C_ready%2C_close%2C_open%2C_resizeMe%2C_renderstart%2Concircled%2Cdrefresh%2Cerefresh&id=I0_1405621934019&pfname=&rpctoken=91543533".replace("XXXXXXX", this.getEncodedURL());
+
             }
 
             //an hash table for a few of categories described in Kodcu.com
@@ -171,8 +196,8 @@
 
                 var url = new Object();
                 url["yazılım"] = "yazilim";
-                url["yazılar"]  = "yazilar";
-                url["kitap"]    = "kitap-2";
+                url["yazılar"] = "yazilar";
+                url["kitap"] = "kitap-2";
                 url["agile"] = "agile-2";
                 url["java"] = "yazilar/java";
                 url["kanban"] = "kanban";
@@ -204,6 +229,7 @@
                 url["spring çatısı"] = "spring-catisi";
                 url["sunucu"] = "sunucu";
                 url["girisimcilik"] = "girisimcilik";
+                url["girişimcilik"] = "girisimcilik";
                 url["web intelligence"] = "web-intelligence";
                 url["yapay zeka"] = "yapay-zeka";
                 url["java enterprise edition"] = "java-enterprise-edition";
@@ -211,17 +237,18 @@
                 url["android"] = "yazilar/android-yazilar";
                 url["javaserver faces"] = "javaserver-faces";
                 url["is zekasi"] = "yazilar/is-zekasi";
+                url["iş zekasi"] = "yazilar/is-zekasi";
                 url["php"] = "yazilar/php";
                 url["jpa"] = "jpa-2";
 
-                this.getValue = function(key) {
+                this.getValue = function (key) {
                     return url[key];// can return undefined!
                 }
 
             }
 
             //initialize the bookmark(s) that include(s) in the other bookmarks folder
-            function setBookmarks(href,index) {
+            function setBookmarks(href, index) {
                 chrome.bookmarks.search(href, function (bookmarkTreeNodes) {
                     var book = {};
                     if (bookmarkTreeNodes.length > 0) {
