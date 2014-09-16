d146fd7d98427a444d551f4666ff753ff7575cee
ozler.hakan <ozler.hakan@gmail.com>
2014-09-16 17:21:56 +0300

    Tr month abbreviations

---
+++ js/monthAbbr.js
@@ -0,0 +1,42 @@
+﻿//get the turkish month abbreviation
+function getMonthAbbreviation(month) {
+    switch (month) {
+        case "Jan":
+            return "Oca";
+            break;
+        case "Feb":
+            return "&#350;ub";
+            break;
+        case "Mar":
+            return "Mart";
+            break;
+        case "Apr":
+            return "Nis";
+            break;
+        case "May":
+            return "May";
+            break;
+        case "Jun":
+            return "Haz";
+            break;
+        case "Jul":
+            return "Tem";
+            break;
+        case "Aug":
+            return "A&#287;u";
+            break;
+        case "Sep":
+        case "Sept":
+            return "Eyl";
+            break;
+        case "Oct":
+            return "Eki";
+            break;
+        case "Nov":
+            return "Kas";
+            break;
+        case "Dec":
+            return "Ara";
+            break;
+    }
+}
