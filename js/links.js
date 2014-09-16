51f3d8a91d53e6affb47e0dde88eb4ff5e64f4e6
ozler.hakan <ozler.hakan@gmail.com>
2014-09-16 17:19:08 +0300

    all the existing mouse events

---
+++ js/links.js
@@ -0,0 +1,41 @@
+﻿$(document).ready(function () {
+    
+    $('a.more-link,a.catehref,ul li a').live('click', function () {
+        chrome.tabs.create({
+            'windowId': chrome.windows.WINDOW_ID_CURRENT,
+            'url': $(this).attr('href'),
+            'selected': true
+        });
+    });
+
+    $('.property').live('click', function () {
+        var navi = $('.autonavi ul');
+        var pixel = $(navi).css('margin-right');
+        if (pixel === "-28px") {
+            $(navi).css({ 'margin-right': '-160px' });
+            $('.property').css({ 'background-image': 'url(/images/right.png)' });
+        } else {
+            $(navi).css({ 'margin-right': '-28px' });
+            $('.property').css({ 'background-image': 'url(/images/left.png)' });
+        }
+    });
+
+    $('.read').live('click', function () {
+        chrome.tabs.create({
+            'windowId': chrome.windows.WINDOW_ID_CURRENT,
+            'url': $(this).attr('href'),
+            'selected': false
+        });
+    });
+
+    $('.base').live('mouseover', function () {
+        var height = $(this).height();
+        $(this).find("a").css({ 'height': height + 'px' });
+        $(this).find("a").css({ 'display': 'block' });
+        $(this).css({ 'opacity': '0.8' });
+    }).live('mouseout', function () {
+        $(this).find("a").css({ 'height': '0px' });
+        $(this).find("a").css({ 'display': 'none' });
+        $(this).css({ 'opacity': '1', });
+    });
+});
