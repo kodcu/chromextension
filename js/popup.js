6a3bc7de889bb17c9df929561e1d5a9db5f18f81
ozler.hakan <ozler.hakan@gmail.com>
2014-09-16 17:22:42 +0300

    generate a popup window for linkedin

---
+++ js/popup.js
@@ -0,0 +1,13 @@
+﻿
+$(document).ready(function () {
+
+    $('.lIn').live('click', function () {
+        createPopup($(this).attr('href'));
+    });
+    
+    function createPopup(href) {
+        var left = (screen.width / 2) - (600 / 2);
+        var top = (screen.height / 2) - (600 / 2);
+        var popup = open(href, "Share", 'width=600,height=600,status=yes,toolbar=no,menubar=no,copyhistory=no, top=' + top + ', left=' + left + '"');
+    }
+});
